f508a2cd5e9fdb27e4fa37f4db37fa2875f03b63
gaby <student29.gs4t@gmail.com>
2025-05-31 14:49:15 -0700

    basic javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+let name= "Gaby";
+let age= 14;
+let likeshushi= true;
+
+function greeting(name) {
+console.log("Hello",name);
+}
+
+greeting("Gaby");
+
+let newName = document.getElementByid("greeter").value
